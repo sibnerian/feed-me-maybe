878629ed8f579759bfbf33af4ccc13606856ee2e
Ian Sibner <sibnerian@gmail.com>
2013-03-30 15:38:39 -0400

    Did some magic

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FoodFinder::Application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['app14249328@heroku.com'],
+    :password       => ENV['5lvtybk3'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+}
