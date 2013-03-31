15f8ff1e940e66387e764f0284aea1d574333d21
Ian Sibner <sibnerian@gmail.com>
2013-03-31 14:01:01 -0400

    Attempted to help devise out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ FoodFinder::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => 'your.websitedomain.com' }
+  ActionMailer::Base.smtp_settings = {
+      :address        => "smtp.sendgrid.net",
+      :port           => "25",
+      :authentication => :plain,
+      :user_name      => ENV['app14249328@heroku.com'],
+      :password       => ENV['5lvtybk3'],
+      :domain         => ENV['heroku.com'],
+      :enable_starttls_auto => true
+  }
+
 
 end
 
