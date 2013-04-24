f59bfcb142df83cfaa5d529d7611df0377d5b1d7
Ian Sibner <sibnerian@gmail.com>
2013-04-24 01:18:52 -0400

    Model spec, what's up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
-require 'spec_helper'
+require "spec_helper"
 
+describe "New user" do
+  it "should not sign up without required fields" do
+    robbo = User.new(name:"Robert Paulson")
 
-describe User do
+    expect(robbo.valid?).to eq(false)
+  end
 
+  it "should be able to sign up WITH required fields" do
+    johnmadden = User.new(name: "john madden", password: "secret", email: "has_an_email@email.com")
 
+    expect(johnmadden.valid?).to eq(true)
+  end
 end
